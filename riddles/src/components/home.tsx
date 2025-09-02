a4b9a3c5e4c5d6d5e998f44274893690e8a6bdb6
Yosef Steinberg <steinbergyosef@gmail.com>
2025-09-02 15:10:14 +0300

    added

---
+++ riddles/src/components/home.tsx
@@ -0,0 +1,13 @@
+import '../style/home.css'
+
+export default function Home(){
+    return(
+        <>
+        <div className="topRow">
+        <h3>Home Page</h3>
+        <h4>Login</h4>
+        </div>
+
+        </>
+    )
+}
